48f0985752d06c8df32ebb197d3f7fa1b3e1d976
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2023-03-13 12:44:34 +0300

    safe area view

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import Homescreen from './screens/Homescreen';
+import { store } from './Store';
 
 export default function App() {
   return (
     <Provider store={store}>
-    <View style={styles.container}>
-      <Text>My Uber clone</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Homescreen />
     </Provider>
   );
 }
---
+++ screens/Homescreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View,SafeAreaView } from 'react-native'
+import React from 'react'
+
+const Homescreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>Homescreen</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Homescreen
+
+const styles = StyleSheet.create({})
